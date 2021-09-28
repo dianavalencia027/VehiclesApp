9254177be5ddc994b124a92a84514a2cc1e75840
Diana Valencia <dianavalencia027@gmail.com>
2021-09-27 20:43:33 -0500

    041 - Starting with CRUD of procedures

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:vehicles_app/models/token.dart';
 import 'package:vehicles_app/screens/login_screen.dart';
+import 'package:vehicles_app/screens/procedures_screen.dart';
 
 
 class HomeScreen extends StatefulWidget {
@@ -77,7 +78,14 @@ class _HomeScreenState extends State<HomeScreen> {
           ListTile(
             leading: Icon(Icons.precision_manufacturing),
             title: const Text('Procedimientos'),
-            onTap: () {}, 
+            onTap: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) => ProceduresScreen(token: widget.token,)
+                )
+              );
+            }, 
           ),
           ListTile(
             leading: Icon(Icons.badge),
