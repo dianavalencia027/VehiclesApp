a91164843b77c1399dc5c89c160ec7b317441d10
Diana Valencia <dianavalencia027@gmail.com>
2021-10-07 20:09:28 -0500

    054 - Update user_screen with user info

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'package:vehicles_app/components/loader_component.dart';
 import 'package:vehicles_app/helpers/api_helper.dart';
+import 'package:vehicles_app/models/document_type.dart';
 import 'package:vehicles_app/models/response.dart';
 import 'package:vehicles_app/models/token.dart';
 import 'package:vehicles_app/models/user.dart';
@@ -27,11 +28,56 @@ class _UserScreenState extends State<UserScreen> {
   bool _firstNameShowError = false;
   TextEditingController _firstNameController = TextEditingController();
 
+  String _lastName = '';
+  String _lastNameError = '';
+  bool _lastNameShowError = false;
+  TextEditingController _lastNameController = TextEditingController();
+
+  DocumentType _documentType = DocumentType(id: 0, description: '');
+  List<DocumentType> _documentTypes = [];
+
+  String _document = '';
+  String _documentError = '';
+  bool _documentShowError = false;
+  TextEditingController _documentController = TextEditingController();
+
+  String _address = '';
+  String _addressError = '';
+  bool _addressShowError = false;
+  TextEditingController _addressController = TextEditingController();
+
+  String _email = '';
+  String _emailError = '';
+  bool _emailShowError = false;
+  TextEditingController _emailController = TextEditingController();
+
+  String _phoneNumber = '';
+  String _phoneNumberError = '';
+  bool _phoneNumberShowError = false;
+  TextEditingController _phoneNumberController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
     _firstName = widget.user.fullName;
     _firstNameController.text = _firstName;
+
+    _lastName = widget.user.lastName;
+    _lastNameController.text = _lastName;
+
+    _documentType = widget.user.documentType;
+
+    _document = widget.user.document;
+    _documentController.text = _document;
+
+    _address = widget.user.address;
+    _addressController.text = _address;
+
+    _email = widget.user.email;
+    _emailController.text = _email;
+
+    _phoneNumber = widget.user.phoneNumber;
+    _phoneNumberController.text = _phoneNumber;
   }
 
   @override
@@ -39,84 +85,29 @@ class _UserScreenState extends State<UserScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          widget.user.id.isEmpty 
+          widget.user.id.isEmpty
             ? 'Nuevo usuario' 
             : widget.user.fullName
         ),
       ),
       body: Stack(
         children: [
-          Column(
-            children: <Widget>[
-              _showFirstName(),
-              _showButtons(),
-            ],
-          ),
-          _showLoader ? LoaderComponent(text: 'Por favor espere...',) : Container(),
-        ],
-      ),
-    );
-  }
-
-  Widget _showFirstName() {
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: TextField(
-        autofocus: true,
-        controller: _firstNameController,
-        decoration: InputDecoration(
-          hintText: 'Ingresa nombres...',
-          labelText: 'Nombres',
-          errorText: _firstNameShowError ? _firstNameError : null,
-          suffixIcon: Icon(Icons.person),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10)
-          ),
-        ),
-        onChanged: (value) {
-          _firstName = value;
-        },
-      ),
-    );
-  }
-
-  Widget _showButtons() {
-    return Container(
-      margin: EdgeInsets.only(left: 10, right: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Expanded(
-            child: ElevatedButton(
-              child: Text('Guardar'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                  (Set<MaterialState> states) {
-                    return Color(0xFF120E43);
-                  }
-                ),
-              ),
-              onPressed: () => _save(), 
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                _showPhoto(),
+                _showFirstName(),
+                _showLastName(),
+                _showDocumentType(),
+                _showDocument(),
+                _showEmail(),
+                _showAddress(),
+                _showPhoneNumber(),
+                _showButtons(),
+              ],
             ),
           ),
-          widget.user.id.isEmpty 
-            ? Container() 
-            : SizedBox(width: 20,),
-          widget.user.id.isEmpty 
-            ? Container() 
-            : Expanded(
-                child: ElevatedButton(
-                  child: Text('Borrar'),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                      (Set<MaterialState> states) {
-                        return Color(0xFFB4161B);
-                      }
-                    ),
-                  ),
-                  onPressed: () => _confirmDelete(), 
-              ),
-          ),
+          _showLoader ? LoaderComponent(text: 'Por favor espere...',) : Container(),
         ],
       ),
     );
@@ -259,6 +250,206 @@ class _UserScreenState extends State<UserScreen> {
     }
 
     Navigator.pop(context, 'yes');
+  }
+
+  Widget _showPhoto() {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: widget.user.id.isEmpty 
+        ? Image(
+            image: AssetImage('assets/noimage.png'),
+            height: 160,
+            width: 160,
+          ) 
+        : ClipRRect(
+            borderRadius: BorderRadius.circular(80),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/vehicles_logo.png'), 
+              image: NetworkImage(widget.user.imageFullPath),
+              width: 160,
+              height: 160,
+              fit: BoxFit.cover
+            ),
+        ),
+    );
+  }
+
+  Widget _showFirstName() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        autofocus: true,
+        controller: _firstNameController,
+        decoration: InputDecoration(
+          hintText: 'Ingresa nombres...',
+          labelText: 'Nombres',
+          errorText: _firstNameShowError ? _firstNameError : null,
+          suffixIcon: Icon(Icons.person),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value) {
+          _firstName = value;
+        },
+      ),
+    );
+  }
+  
+  Widget _showLastName() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        controller: _lastNameController,
+        decoration: InputDecoration(
+          hintText: 'Ingresa apellidos...',
+          labelText: 'Apellidos',
+          errorText: _lastNameShowError ? _lastNameError : null,
+          suffixIcon: Icon(Icons.person),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value) {
+          _lastName = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showDocumentType() {
+    return Container(
+
+    );
+  }
+
+  Widget _showDocument() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        controller: _documentController,
+        decoration: InputDecoration(
+          hintText: 'Ingresa documento...',
+          labelText: 'Documento',
+          errorText: _documentShowError ? _documentError : null,
+          suffixIcon: Icon(Icons.assignment_ind),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value) {
+          _document = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showEmail() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        controller: _emailController,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'Ingresa email...',
+          labelText: 'Email',
+          errorText: _emailShowError ? _emailError : null,
+          suffixIcon: Icon(Icons.email),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value) {
+          _email = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showAddress() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        controller: _addressController,
+        keyboardType: TextInputType.streetAddress,
+        decoration: InputDecoration(
+          hintText: 'Ingresa dirección...',
+          labelText: 'Dirección',
+          errorText: _addressShowError ? _addressError : null,
+          suffixIcon: Icon(Icons.home),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value) {
+          _address = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showPhoneNumber() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        controller: _phoneNumberController,
+        keyboardType: TextInputType.phone,
+        decoration: InputDecoration(
+          hintText: 'Ingresa teléfono...',
+          labelText: 'Teléfono',
+          errorText: _phoneNumberShowError ? _phoneNumberError : null,
+          suffixIcon: Icon(Icons.phone),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value) {
+          _phoneNumber = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showButtons() {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Expanded(
+            child: ElevatedButton(
+              child: Text('Guardar'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states) {
+                    return Color(0xFF120E43);
+                  }
+                ),
+              ),
+              onPressed: () => _save(), 
+            ),
+          ),
+          widget.user.id.isEmpty 
+            ? Container() 
+            : SizedBox(width: 20,),
+          widget.user.id.isEmpty 
+            ? Container() 
+            : Expanded(
+                child: ElevatedButton(
+                  child: Text('Borrar'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                        return Color(0xFFB4161B);
+                      }
+                    ),
+                  ),
+                  onPressed: () => _confirmDelete(), 
+              ),
+          ),
+        ],
+      ),
+    );
   }
 
 
