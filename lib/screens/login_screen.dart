0ecff060f45e1783f476124d255346794ec1e593
Diana Valencia <dianavalencia027@gmail.com>
2021-10-08 07:06:53 -0500

    056 - Update LoginScreen & _showDocumentType comboBox

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -36,16 +36,19 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _showlogo(),
-              SizedBox(height: 20,),
-              _showEmail(),
-              _showPassword(),
-              _showRememberme(),
-              _showButtons(),
-            ],  
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 40,),
+                _showLogo(),
+                SizedBox(height: 20,),
+                _showEmail(),
+                _showPassword(),
+                _showRememberme(),
+                _showButtons(),
+              ],
+            ),
           ),
           _showLoader ? LoaderComponent(text: 'Por favor espere...') : Container(),
         ],
@@ -53,7 +56,7 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Widget _showlogo() {
+  Widget _showLogo() {
     return Image(
       image: AssetImage('assets/vehicles_logo.png'),
       width: 300,
